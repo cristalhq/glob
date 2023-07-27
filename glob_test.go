700028330f6423d302cd48f764c4f20ed3066601
Oleg Kovalov <oleg@hey.com>
2023-07-27 18:32:10 +0200

    Minimal benchmark

---
+++ glob_test.go
@@ -0,0 +1,42 @@
+package glob
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func BenchmarkCompile(b *testing.B) {
+	pattern := `foo/**/*.go`
+
+	for i := 0; i < b.N; i++ {
+		g, err := Compile(pattern)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if g == nil {
+			b.Fatal("have nil glob")
+		}
+	}
+}
+
+func BenchmarkMatch(b *testing.B) {
+	pattern := `foo/**/*.go`
+	g, err := Compile(pattern)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var count int64
+	for i := 0; i < b.N; i++ {
+		ok := g.Match(`foo/bar/main.go`)
+		if ok {
+			count++
+		}
+	}
+}
+
+func sink(v int64) {
+	if rand.Float32() > 1 {
+		panic(v)
+	}
+}
